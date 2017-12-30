d2f6c23baa8887fded1d7f256329aa014bb63a87
darkdrgn2k <darkdrgn2k@networkedserver.net>
2017-12-29 23:12:29 -0500

    Copy Paste without change oops

--- tunnel/IpTunnel.h
+++ tunnel/IpTunnel.h
@@ -47,7 +47,7 @@ struct IpTunnel_Connection
     uint8_t routedIp6Prefix;
 
     /** The IPv6 prefix length in, in bits, defining netmask. 0xff if not used. */
-    uint8_t connectionIp6Alloc;
+    uint8_t routedIp6Alloc;
 
     /** The IPv4 address prefix length, in bits. Defaults to 32 if none was assigned. */
     uint8_t connectionIp4Prefix;
